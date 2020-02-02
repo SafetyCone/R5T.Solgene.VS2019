eaf4a14baf6292473bfd7552329eb9312121d677
MinexAutomation <minex.automation@gmail.com>
2020-02-01 16:02:05 -0800

    Added VisualStudioSolutionFileGenerator implementation of IVisualStudioSolutionFileGenerator.

---
+++ source/R5T.Solgene.VS2019/Code/Services/Implementations/VisualStudioSolutionFileGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Solgene.VS2019
+{
+    public class VisualStudioSolutionFileGenerator : IVisualStudioSolutionFileGenerator
+    {
+        public SolutionFile GenerateSolutionFile()
+        {
+            var solutionFile = SolutionFileGenerator.NewVisualStudio2019();
+            return solutionFile;
+        }
+    }
+}
