6e16b05677ad311219d63c73d5eaf0ef1ec8b87b
MinexAutomation <minex.automation@gmail.com>
2020-02-01 18:42:42 -0800

    Added VS2017-specific IServiceCollection extensions.

--- source/R5T.Solgene.VS2019/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Solgene.VS2019/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -10,11 +10,34 @@ namespace R5T.Solgene.VS2019
     public static class IServiceCollectionExtensions
     {
         /// <summary>
+        /// Adds the <see cref="VisualStudio2019SolutionFileGenerator"/> implementation of <see cref="IVisualStudio2019SolutionFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudio2019SolutionFileGenerator(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudio2019SolutionFileGenerator, VisualStudio2019SolutionFileGenerator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudio2019SolutionFileGenerator"/> implementation of <see cref="IVisualStudio2019SolutionFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudio2019SolutionFileGenerator> AddVisualStudio2019SolutionFileGeneratorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudio2019SolutionFileGenerator>(() => services.AddVisualStudio2019SolutionFileGenerator());
+            return serviceAction;
+        }
+
+        /// <summary>
         /// Adds the <see cref="VisualStudioSolutionFileGenerator"/> implementation of <see cref="IVisualStudioSolutionFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
         /// </summary>
-        public static IServiceCollection AddVS2019VisualStudioSolutionFileGenerator(this IServiceCollection services)
+        public static IServiceCollection AddVisualStudioSolutionFileGenerator(this IServiceCollection services,
+            ServiceAction<IVisualStudio2019SolutionFileGenerator> addVisualStudio2019SolutionFileGenerator)
         {
-            services.AddSingleton<IVisualStudioSolutionFileGenerator, VisualStudioSolutionFileGenerator>();
+            services
+                .AddSingleton<IVisualStudioSolutionFileGenerator, VisualStudioSolutionFileGenerator>()
+                .RunServiceAction(addVisualStudio2019SolutionFileGenerator)
+                ;
 
             return services;
         }
@@ -22,9 +45,10 @@ namespace R5T.Solgene.VS2019
         /// <summary>
         /// Adds the <see cref="VisualStudioSolutionFileGenerator"/> implementation of <see cref="IVisualStudioSolutionFileGenerator"/> as a <see cref="ServiceLifetime.Singleton"/>.
         /// </summary>
-        public static ServiceAction<IVisualStudioSolutionFileGenerator> AddVS2019VisualStudioSolutionFileGeneratorAction(this IServiceCollection services)
+        public static ServiceAction<IVisualStudioSolutionFileGenerator> AddVisualStudioSolutionFileGeneratorAction(this IServiceCollection services,
+            ServiceAction<IVisualStudio2019SolutionFileGenerator> addVisualStudio2019SolutionFileGenerator)
         {
-            var serviceAction = new ServiceAction<IVisualStudioSolutionFileGenerator>(() => services.AddVS2019VisualStudioSolutionFileGenerator());
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileGenerator>(() => services.AddVisualStudioSolutionFileGenerator(addVisualStudio2019SolutionFileGenerator));
             return serviceAction;
         }
     }
