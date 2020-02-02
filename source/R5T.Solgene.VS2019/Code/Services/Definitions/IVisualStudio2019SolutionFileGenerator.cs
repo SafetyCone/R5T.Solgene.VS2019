ccb839e78403dc53d78df44aa2fee231a7241188
MinexAutomation <minex.automation@gmail.com>
2020-02-01 18:41:08 -0800

    Added IVisualStudio2019SolutionFileGenerator service definition interface type.

---
+++ source/R5T.Solgene.VS2019/Code/Services/Definitions/IVisualStudio2019SolutionFileGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Solgene.VS2019
+{
+    public interface IVisualStudio2019SolutionFileGenerator
+    {
+        SolutionFile GenerateVisualStudio2019SolutionFile();
+    }
+}
